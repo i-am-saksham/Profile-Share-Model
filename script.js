13916bdc4e967589744e8acca336555bae4b794b
Saksham Bhardwaj <125785732+i-am-saksham@users.noreply.github.com>
2024-09-15 12:47:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 const modal = document.querySelector(".modal");
-// const overlay = document.querySelector(".overlay");
 
-// modal open function
 const openModal=() =>{
     console.log("Modal is open");
-    modal.classList.add("active");/*add css class in an element */
-    // overlay.classList.add("overlayactive");
+    modal.classList.add("active");
 };
 
-// modal close function
 const closeModal=() =>{
     console.log("Modal is close");
     modal.classList.remove("active");
-    // overlay.classList.remove("overlayactive");
 };
 
